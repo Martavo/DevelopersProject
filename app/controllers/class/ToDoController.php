ec550daaa3f63d4158d58706b9a73e59af06e5c8
Diego Chacon <diegdarch@hotmail.com>
2024-01-19 15:44:23 +0100

    metodo searchTask() en clase ToDoController

--- app/controllers/class/ToDoController.php
+++ app/controllers/class/ToDoController.php
@@ -2,9 +2,6 @@
 
 class ToDo
 {
-
-    // PENDIENTES: updateTask(), listTasks()->(creo que seria el mismo getTasks())
-
     protected array $tasks;
 
     public function getTasks()
@@ -14,42 +11,20 @@ class ToDo
         return $this->tasks = $tasks;
     }
 
-    public function deleteTask(int $searchedTaskId)
-    {
-        $tasks = $this->getTasks();
-        $foundTask = false;
-
-        $longArray = count($tasks);
-        $i=0;
-        while($foundTask==false && $i<$longArray)             
-        {
-            if($tasks[$i]["taskId"]==$searchedTaskId)
-            {
-                array_splice($tasks,$i, 1);
-                $foundTask = true;
-            }
-            $i++;
-        }
-        echo $foundTask==false ? "La tarea buscada no se encuentra en nuestra BBDD" : null;
-
-        $this->addJson($tasks);
-    }
-
     public function createTask(Task $task, User $user)
     {
         $tasks = $this->getTasks(); //obtenemos todas las tasks antes de agregar la nueva tarea
 
-        // recogemos los valores de los getters de cada objeto en cada variable
-        $taskId=$task->getTaskId();
-        $user = $user->getNickName();
-        $taskName = $task->getTaskName();
-        $taskType=$task->getTaskType();
-        $creationDate=$task->getCreationDate();
-        $expectedEndDate=$task->getExpectedEndDate();
-        $taskStatus=$task->gettaskStatus();
-
-        // creamos la tarea con los valores de cada variable
-        $newTask = ["taskId"=>$taskId, "usuario"=>$user,"nomTarea"=>$taskName, "taskType"=>$taskType, "creationDate"=>$creationDate, "expectedEndDate"=>$expectedEndDate, "taskStatus"=>$taskStatus];
+        // creamos la tarea nueva con los valores de cada campo
+        $newTask = [
+            // recogemos los valores de los getters y lo pasamos a la clave del array asociativo
+            "taskId"=>$task->getTaskId(), 
+            "user"=>$user->getNickName(),
+            "taskName"=>$task->getTaskName(), 
+            "taskType"=>$task->getTaskType(), 
+            "creationDate"=>$task->getCreationDate(), 
+            "expectedEndDate"=>$task->getExpectedEndDate(), "taskStatus"=>$task->gettaskStatus()
+        ];
 
         // insertamos la tarea en el array de $tasks
         $tasks []= $newTask;
@@ -64,6 +39,48 @@ class ToDo
         file_put_contents(__DIR__ . '../../../models/toDo.json', json_encode($tasks, JSON_PRETTY_PRINT));
     }
 
+    public function deleteTask(int $taskId)
+    {
+        $tasks = $this->getTasks();
+
+        $isFound = false;
+        $longArray = count($tasks);
+        $i=0;
+        while($isFound==false && $i<$longArray)             
+        {
+            if($tasks[$i]["taskId"]==$taskId)
+            {//elimina la posicion de la tarea dentro del array $tasks
+                array_splice($tasks,$i, 1);
+                $isFound = true;//cuando encuentre la tarea dejara de iterar
+            }
+            $i++;
+        }
+
+        $this->addJson($tasks);
+    }
+
+    public function searchTask(int $taskId): array
+    {
+        $tasks = $this->getTasks();
+
+        $isFound = false;
+        $longArray = count($tasks);
+        $i=0;
+        while($isFound==false && $i<$longArray)             
+        {
+            if($tasks[$i]["taskId"]==$taskId)
+            {
+                $taskFound = $tasks[$i];
+                $isFound = true;//cuando encuentre la tarea dejara de iterar
+            }
+            $i++;
+        }
+        return $taskFound;
+    }
+
+    
 }
+
+
 
 
