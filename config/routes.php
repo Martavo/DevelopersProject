a12e49d231b20ae308440f45321eab3942577a1b
Reivax9696 <149475827+Reivax9696@users.noreply.github.com>
2024-01-24 11:40:30 +0100

    Update routes.php
    rutas actualizadas

--- config/routes.php
+++ config/routes.php
@@ -3,12 +3,21 @@
 /**
  * Used to define the routes in the system.
  * 
- * A route should be defined with a key matching the URL and an controller#action-to-call method. E.g.:
+ * A route should be defined with a key matching the URL and an cont =roller#action-to-call method. E.g.:
  * 
  * '/' => 'index#index',
  * '/calendar' => 'calendar#index'
  */
 $routes = array(
-	'/test' => 'test#index'
+	//Create
+	'/createUserForm' => 'application#createUserForm_View',
+	//Delete
+	'/deleteUserProfile' => 'application#deleteUserProfile_View',
+	'/loginUsersForm' => 'application#loginUsersForm_View',
+	'/updayeUserProfile' => 'application#updateUserProfile_View',
+	'/userProfile' => 'application#userProfile_View',
+	
+
+
 	
 );
